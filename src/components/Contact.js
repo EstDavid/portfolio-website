16a50752055305ea8a8c9d5e764c9d31877b5e5a
Vandenynas <stlo.subs@gmail.com>
2022-08-06 20:30:36 +0300

    Refactored variables so all data is imported from /assets/copyData.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import { mainLinks } from '../assets/copyData';
 
 const ContactItem = (props) => {
     return (
-        <div class="col-md-4 mt-4 pt-2">
-            <div class="contact-detail text-center">
-                <i class={`bi bi-${props.iconText}-fill`} style={{fontSize: "3rem"}}></i>
-                <div class="content mt-4">
-                    <h5 class="title text-uppercase">{props.title}</h5>
-                    <p class="text-muted">{props.extraText}</p>
-                    <a href={props.linkURL} class="text-primary">{props.contactData}</a>
+        <div className="col-md-4 mt-4 pt-2">
+            <div className="contact-detail text-center">
+                <i className={`bi bi-${props.iconText}-fill`} style={{fontSize: "3rem"}}></i>
+                <div className="content mt-4">
+                    <h5 className="title text-uppercase">{props.title}</h5>
+                    <p className="text-muted">{props.extraText}</p>
+                    <a href={props.linkURL} className="text-primary">{props.contactData}</a>
                 </div>
             </div>
         </div>
@@ -20,17 +21,17 @@ const Contact = () => {
     return(
         <section className="section bg-light" id="contact">
             <div className="container">
-                <div class="row justify-content-center">
-                    <div class="col-12 text-center">
-                        <div class="section-title">
-                            <div class="position-relative">
-                                <h2 class="title text-uppercase mb-4">Contact Me</h2>
+                <div className="row justify-content-center">
+                    <div className="col-12 text-center">
+                        <div className="section-title">
+                            <div className="position-relative">
+                                <h2 className="title text-uppercase mb-4">Contact Me</h2>
                             </div>
                         </div>
                     </div>
                 </div>
 
-                <div class="row">
+                <div className="row">
                 <ContactItem
                     title="Phone"
                     iconText="telephone"
@@ -42,7 +43,7 @@ const Contact = () => {
                     iconText="envelope"
                     extraText="Send me an email to:"
                     contactData="estdav@protonmail.com"
-                    linkURL="mailto:estdav@protonmail.com" />
+                    linkURL={mainLinks.email} />
                 <ContactItem 
                     title="Location"
                     iconText="geo-alt"
